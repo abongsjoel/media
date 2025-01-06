e021e111c2dc5ab6729d2e75928192f2d18815ab
Joel Chi <abongsjoel@gmail.com>
2025-01-06 03:39:34 +0100

    Add Skeleton component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,20 +1,27 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store/";
+import Skeleton from "./Skeleton";
 
 function UsersList() {
   const dispatch = useDispatch();
 
-  const loading = useSelector((rootState) => rootState.users.isLoading);
-  const users = useSelector((rootState) => rootState.users.data);
-  const error = useSelector((rootState) => rootState.users.error);
-  console.log({ loading, users, error });
+  const { isLoading, data, error } = useSelector(
+    (rootState) => rootState.users
+  );
+  console.log({ isLoading, data, error });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <div>UsersList</div>;
+  return isLoading ? (
+    <Skeleton />
+  ) : (
+    <div>
+      <Skeleton />
+    </div>
+  );
 }
 
 export default UsersList;
