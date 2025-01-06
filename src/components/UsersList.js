ff8b1132b441145b118c3d882c83a529f156985a
Joel Chi <abongsjoel@gmail.com>
2025-01-06 03:14:27 +0100

    Fetch list of users from json-server

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store/";
+
 function UsersList() {
+  const dispatch = useDispatch();
+
+  const loading = useSelector((rootState) => rootState.users.isLoading);
+  const users = useSelector((rootState) => rootState.users.data);
+  const error = useSelector((rootState) => rootState.users.error);
+  console.log({ loading, users, error });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <div>UsersList</div>;
 }
 
